af4fbed8a2952310585e450c18cdc716128a2cb6
Tess Huang <tesshuangxj@yahoo.com>
2021-04-23 14:59:48 -0700

    Setup basic server

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "https://css-tricks.com",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
