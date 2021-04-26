ff822dfb3b3a9e78c34acdaf226850770d5ffdbf
Tess Huang <tesshuangxj@yahoo.com>
2021-04-26 14:47:26 -0700

    Update server using ejs template file

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "https://css-tricks.com",
   "9sm5xK": "http://www.google.com"
@@ -11,12 +13,19 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {
