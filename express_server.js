2162b288b535a70bf089cba09c7e51f4e59400f7
Tess Huang <tesshuangxj@yahoo.com>
2021-04-26 16:50:59 -0700

    Add urls new template and update new route

--- express_server.js
+++ express_server.js
@@ -18,6 +18,10 @@ app.get("/urls", (req, res) => {
   res.render("pages/urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("pages/urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("pages/urls_show", templateVars);
